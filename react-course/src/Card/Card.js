e182ea8ff545002ceab1a1bd912f5b858f27c4ff
AKavalchuk <AKavalchuk@gomel.iba.by>
2020-02-25 14:21:31 +0300

    task1

--- react-course/src/Card/Card.js
+++ react-course/src/Card/Card.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import './Card.css';
 
-const card = ( props ) => {
-	return (
-		<div className="Card">
-			<div>{props.caption}</div>
-			<div className="Card-line" />
-			<div className="Card-text">{props.description}</div>
-		</div>
-	);
+const Card = props => {
+    return (
+        <div className="Card">
+            <div>{props.caption}</div>
+            <hr />
+            <div className="Card-text">{props.description}</div>
+        </div>
+    );
 };
 
-export default card;
+export default Card;
