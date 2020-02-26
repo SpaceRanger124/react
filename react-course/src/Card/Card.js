be45e222dd7b927f87b2b462ca3d6555a2b2a521
AKavalchuk <AKavalchuk@gomel.iba.by>
2020-02-26 14:21:47 +0300

    task2

--- react-course/src/Card/Card.js
+++ react-course/src/Card/Card.js
@@ -2,25 +2,30 @@ import React, { Component } from 'react';
 import './Card.css';
 
 class Card extends Component {
-  state = { isCheckboxChecked: false }
+    state = { isCheckboxChecked: false };
 
-  changeStyles = () => {
-    this.setState({isCheckboxChecked: !this.state.isCheckboxChecked})
-  }
+    changeStyles = () => {
+        this.setState({ isCheckboxChecked: !this.state.isCheckboxChecked });
+    };
 
-  render() {
-    let styleClass = this.state.isCheckboxChecked ? "Card-checked" : "Card"
+    render() {
+        let styleClass = this.state.isCheckboxChecked ? 'Card-checked' : 'Card';
 
-    return (
-      <div className={styleClass}>
-        <input type="checkbox" className="Card-checkbox" onChange={this.changeStyles}/>
-        <div>{this.props.caption}</div>
-        <hr />
-        <div className="Card-text">{this.props.description}</div>
-      </div>
-    );
-  }
-
+        return (
+            <div className={styleClass}>
+                <div className="Card-header">
+                    <span>{this.props.caption}</span>
+                    <input
+                        type="checkbox"
+                        className="Card-checkbox"
+                        onChange={this.changeStyles}
+                    />
+                </div>
+                <hr />
+                <div className="Card-text">{this.props.description}</div>
+            </div>
+        );
+    }
 }
 
 export default Card;
