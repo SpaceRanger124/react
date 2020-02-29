4ebf5cded6460c8a6077c52dadbb8dfe2392aef0
Aliaksandr Kavalchuk <AKavalchuk@gomel.iba.by>
2020-02-29 21:33:35 +0300

    task4

--- react-course/src/App.js
+++ react-course/src/App.js
@@ -1,16 +1,71 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Card from './Card/Card';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        Header
-      </header>
-	  <Card caption="Mars" description="This is a planet in the Solar System" />
-    </div>
-  );
+class App extends Component {
+	
+	state = {
+		cards: [
+			{caption: "Mercury", description: "This is the first planet from the Sun.", editMode: false },
+			{caption: "Venus", description: "This is the second planet from the Sun.", editMode: false },
+			{caption: "Earth", description: "This is the third planet from the Sun.", editMode: false },
+			{caption: "Mars", description: "This is the fourth planet from the Sun.", editMode: false },
+			{caption: "Jupiter", description: "This is the fifth planet from the Sun.", editMode: false },
+			{caption: "Saturn", description: "This is the sixth planet from the Sun.", editMode: false },
+			{caption: "Uranus", description: "This is the seventh planet from the Sun.", editMode: false },
+			{caption: "Neptune", description: "This is the eighth planet from the Sun.", editMode: false }
+		],
+		readOnly: false
+	};
+	
+	switchReadOnly = () => {
+		const cards = this.state.cards.slice();
+		for(let i = 0; i < cards.length; i++) {
+			cards[i].editMode = false;
+		}
+		this.setState({
+			cards: cards,
+			readOnly: !this.state.readOnly
+		});
+	}
+	
+	switchEditMode = (index) => {
+		const cards = this.state.cards.slice();
+		cards[index].editMode = !cards[index].editMode;
+		this.setState({
+			cards: cards
+		});
+	}
+	
+	render() {
+		const cards = this.state.cards.map((card, index) => {
+			return (
+				<Card caption={card.caption}
+				description={card.description}
+				readOnly={this.state.readOnly}
+				editMode={card.editMode}
+				switchEditMode={() => this.switchEditMode(index)}
+				key={index}
+				/>
+			);
+		});
+		return (
+			<div className="App">
+				<header className="App-header">
+					The Solar System
+				</header>
+				<input
+					className="App-checkbox"
+                    type="checkbox"
+					checked={this.state.readOnly}
+					onChange={this.switchReadOnly}
+                />
+				<span className="App-checkbox-label">Read only</span>
+				{cards}
+			</div>
+		);
+	}
+	
 }
 
 export default App;
--- react-course/src/Card/Card.js
+++ react-course/src/Card/Card.js
@@ -4,12 +4,12 @@ import { MdEdit, MdSave, MdCancel } from 'react-icons/md';
 import ContentEditable from 'react-contenteditable'
 
 class Card extends Component {
-    state = {
+	state = {
         isCheckboxChecked: false,
-        isEditMode: false,
         caption: this.props.caption,
         description: this.props.description
     };
+	
     temporaryCaption = this.state.caption;
     temporaryDescription = this.state.description;
 
@@ -26,55 +26,67 @@ class Card extends Component {
     }
 
     editCard = () => {
+		this.props.switchEditMode();
         this.setState({
-            isEditMode: true,
             isCheckboxChecked: false
         });
     }
 
     saveChanges = () => {
+		this.props.switchEditMode();
         this.setState({
-            isEditMode: false,
             caption: this.temporaryCaption,
             description: this.temporaryDescription
         });
     }
 
     cancelChanges = () => {
-         this.setState({
-            isEditMode: false,
-         });
+		this.props.switchEditMode();
     }
 
     render() {
         let styleClass = this.state.isCheckboxChecked ? 'Card-checked' : 'Card';
+		let saveButton = null;
+		let cancelButton = null;
+		let editButton = null;
+		if (!this.props.readOnly) {
+			saveButton = (
+				<MdSave
+                    className="Card-icon"
+                    onClick={this.saveChanges}
+                />
+			);
+			cancelButton = (
+				<MdCancel
+                    className="Card-icon"
+                    onClick={this.cancelChanges}
+                />
+			);
+			editButton = (
+				<MdEdit
+                    className="Card-icon"
+                    onClick={() => this.editCard()}
+                />
+			);
+		}
 
         return (
             <div className={styleClass}>
                 <div className="Card-header">
                     <ContentEditable
-                        disabled={!this.state.isEditMode}
+                        disabled={!this.props.editMode}
                         onChange={this.handleCaptionChange}
                         html={this.state.caption}
                     />
                     <span>
-                        {this.state.isEditMode ? (
+                        {this.props.editMode ? (
                             <React.Fragment>
-                                <MdSave
-                                    className="Card-icon"
-                                    onClick={this.saveChanges}
-                                />
-                                <MdCancel
-                                    className="Card-icon"
-                                    onClick={this.cancelChanges}
-                                />
+								{saveButton}
+								{cancelButton}
                             </React.Fragment>
                         ) : (
                             <React.Fragment>
-                                <MdEdit
-                                    className="Card-icon"
-                                    onClick={this.editCard}
-                                />
+								{editButton}
                                 <input
                                     className="Card-checkbox"
                                     type="checkbox"
@@ -88,7 +100,7 @@ class Card extends Component {
                 <hr />
                 <ContentEditable
                     className="Card-text"
-                    disabled={!this.state.isEditMode}
+                    disabled={!this.props.editMode}
                     onChange={this.handleDescriptionChange}
                     html={this.state.description}
                 />
