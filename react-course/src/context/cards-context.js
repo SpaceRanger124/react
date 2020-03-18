bb56fc7fdf303a576e004e4ef6ab8275f3ed9048
AKavalchuk <AKavalchuk@gomel.iba.by>
2020-03-18 09:20:27 +0300

    #Task10

--- react-course/src/context/cards-context.js
+++ react-course/src/context/cards-context.js
@@ -1,61 +1,13 @@
 import React, { Component } from 'react';
 import { v1 as uuidv1 } from 'uuid';
+import axios from 'axios';
 
 const CardContext = React.createContext();
 const { Provider, Consumer } = CardContext;
 
 class CardsProvider extends Component {
     state = {
-        cards: [
-            {
-                id: uuidv1(),
-                caption: "Mercury",
-                description: "This is the first planet from the Sun.",
-                isSelected: false
-            },
-            {
-                id: uuidv1(),
-                caption: "Venus",
-                description: "This is the second planet from the Sun.",
-                isSelected: false
-            },
-            {
-                id: uuidv1(),
-                caption: "Earth",
-                description: "This is the third planet from the Sun.",
-                isSelected: false
-            },
-            {
-                id: uuidv1(),
-                caption: "Mars",
-                description: "This is the fourth planet from the Sun.",
-                isSelected: false
-            },
-            {
-                id: uuidv1(),
-                caption: "Jupiter",
-                description: "This is the fifth planet from the Sun.",
-                isSelected: false
-            },
-            {
-                id: uuidv1(),
-                caption: "Saturn",
-                description: "This is the sixth planet from the Sun.",
-                isSelected: false
-            },
-            {
-                id: uuidv1(),
-                caption: "Uranus",
-                description: "This is the seventh planet from the Sun.",
-                isSelected: false
-            },
-            {
-                id: uuidv1(),
-                caption: "Neptune",
-                description: "This is the eighth planet from the Sun.",
-                isSelected: false
-            }
-        ],
+        cards: [],
         isAddCardPanelVisible: false
     };
 
@@ -115,6 +67,24 @@ class CardsProvider extends Component {
                 card => !card.isSelected
            )
         });
+	}
+
+	componentDidMount() {
+	    axios.get('https://raw.githubusercontent.com/BrunnerLivio/PokemonDataGraber/master/output.json')
+	        .then(response => {
+                const cards = response.data.slice(0, 15);
+                const processedCards = cards.map(card => {
+                    return {
+                        id: card.Number,
+                        caption: card.Name,
+                        description: card.About
+                    }
+                });
+
+                this.setState({
+                    cards: processedCards
+                });
+	        });
 	}
 
     render() {
