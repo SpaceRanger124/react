d857f3b94143b379f24354d6cc906a3c1d408b57
AKavalchuk <AKavalchuk@gomel.iba.by>
2020-04-14 14:57:11 +0300

    #Task16

--- react-course/src/components/CardList/Card/Card.test.js
+++ react-course/src/components/CardList/Card/Card.test.js
@@ -1,23 +1,41 @@
 import React from 'react';
 
-import { configure, shallow } from 'enzyme';
+import { configure, shallow, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 import Card from './Card';
-import CardBody from './CardBody/CardBody';
-import CardList from '../CardList';
 
 configure({adapter: new Adapter()});
 
 describe('<Card />', () => {
-    it('it should', () => {
-        const wrapper = shallow(<Card />);
-        console.log(wrapper.children().length);
-        expect(wrapper.find(<div/>)).toHaveLength(1);
+
+    let wrapper;
+
+    beforeEach(() => {
+        wrapper = shallow(<Card className='mt-1' readOnly={false} caption='testCaption' description='testDescription' />);
+        jest.useFakeTimers();
     });
 
-    it('it should', () => {
-        const wrapper = shallow(<Card />).dive();
-        expect(wrapper.find(CardList)).toHaveLength(1);
+    it('it should have Loader component', () => {
+        expect(wrapper.find('Loader').length).toEqual(1);
+    });
+
+    it('it should have Card component', () => {
+        jest.advanceTimersByTime(3000);
+        expect(wrapper.find('Card').length).toEqual(1);
+    });
+
+    it('it should have CardHeader component', () => {
+        jest.advanceTimersByTime(3000);
+        let card = wrapper.find('Card').dive();
+        expect(card.find('CardHeader').length).toEqual(1);
+        expect(card.state('caption')).toEqual('testCaption');
+    });
+
+    it('it should have CardBody component', () => {
+        jest.advanceTimersByTime(3000);
+        let card = wrapper.find('Card').dive();
+        expect(card.find('CardBody').length).toEqual(1);
+        expect(card.state('description')).toEqual('testDescription');
     });
 });
